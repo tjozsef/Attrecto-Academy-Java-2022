82b303823073ba8459ed3b5157efe1ed0bf073f0
peter.papai@attrecto.com <peter.papai@attrecto.com>
2022-09-15 14:35:30 +0200

    Első órai anyag

---
+++ src/main/java/com/attrecto/academy/java/courseapp/AttrectoAcademyCourseAppApplication.java
@@ -0,0 +1,13 @@
+package com.attrecto.academy.java.courseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttrectoAcademyCourseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttrectoAcademyCourseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attrecto/academy/java/courseapp/AttrectoAcademyCourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attrecto.academy.java.courseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttrectoAcademyCourseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
